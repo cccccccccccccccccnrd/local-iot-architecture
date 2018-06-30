472d093d1080e82eada494182c0a8cfe62d8e5bd
cnrd <hello@conradweise.com>
2018-06-30 21:53:17 +0200

    fixes

--- index.js
+++ index.js
@@ -166,6 +166,7 @@ board.on('ready', function () {
     client.publish(waterElectricalConductivityTopic, JSON.stringify(waterElectricalConductivityState))
     console.log(JSON.stringify(waterTemperatureState))
     console.log(JSON.stringify(waterElectricalConductivityState))
+    console.log(data)
   })
 
   /* MQTT subscribe handeling */
