7cc9c61425aaaba005b182e941c8746069ae9fa0
cnrd <hello@conradweise.com>
2018-08-16 18:25:20 +0200

    fix

--- public/local-iot-architecture.js
+++ public/local-iot-architecture.js
@@ -267,7 +267,7 @@ connectButton.onclick = function () {
     } else if (topic === iotaMamTopic) {
       iotaMamState = String(message)
       logToTextarea(iotaMamState)
-      console.log(iotaMamState)
+      console.log(String(message))
     }
   })
 }
