1c4a1d675cac7f25578c7e9bafc934c321f54ff9
cnrd <hello@conradweise.com>
2018-08-18 12:11:41 +0200

    fix

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ board.on('ready', function () {
   setInterval(() => {
     const now = new Date()
 
-    if ((now.getHours() === 12 && now.getMinutes() === 10) || (now.getHours() === 18 && now.getMinutes() === 00)) {
+    if ((now.getHours() === 12 && now.getMinutes() === 13) || (now.getHours() === 18 && now.getMinutes() === 00)) {
       relayOxygenpump.open()
           console.log('oxygenpump:', oxygenpumpState)
           setTimeout(() => {
