d3a7a74fc86f0fe2e6f195ffbcedc44ffe8d9763
cnrd <hello@conradweise.com>
2018-09-06 18:45:14 +0200

    fix

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ setup.board.on('ready', function () {
       }, 1 * 60000)
     }
 
-    if ((now.getHours() === 18 && now.getMinutes() === 37) ||
+    if ((now.getHours() === 18 && now.getMinutes() === 47) ||
         (now.getHours() === 22 && now.getMinutes() === 00)) {
       const timestamp = Date.now()
 
